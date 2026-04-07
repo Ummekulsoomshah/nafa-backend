6e9db83d4912ef1834b2083958db7b61639e399b
ummekulsoom <ummekulsoom215@gmail.com>
2026-04-08 04:11:43 +0500

    changes

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -13,6 +13,9 @@ const AppDataSource = new DataSource({
   username: configService.get<string>('DB_USER'),
   password: configService.get<string>('DB_PASS'),
   database: configService.get<string>('DB_NAME'),
+  ssl: {
+    rejectUnauthorized: false,
+  },
   synchronize: false,
   entities: ['**/*.entity.ts'],
  migrations: [isTsEnv ? 'src/db/migrations/*.ts' : 'dist/db/migrations/*.js'],
